47d88109ac6856bd483d1a5b1e3423255c531e56
= <sanjuan.jeancarlo@gmail.com>
2026-01-05 03:03:50 +0800

    feat: update page title to 'Pokemon Mezastar Helper by JCSJ'

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,7 +22,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Pokemon Mezastar Helper by JCSJ',
       },
     ],
     links: [
